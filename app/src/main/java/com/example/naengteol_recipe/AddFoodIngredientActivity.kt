a6556650b8b37001c63f1b7b10afd98ec6a077d8
kimok lee <kmlee@doflab.com>
2024-05-13 14:32:28 +0900

    initialize

---
+++ app/src/main/java/com/example/naengteol_recipe/AddFoodIngredientActivity.kt
@@ -0,0 +1,10 @@
+package com.example.naengteol_recipe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddFoodIngredientsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
