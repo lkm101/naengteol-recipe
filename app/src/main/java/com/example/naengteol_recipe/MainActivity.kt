8c676e3e48b251722ffc35df4aa2335318a5e156
kimok lee <kmlee@doflab.com>
2024-05-15 01:03:07 +0900

    재료 입력, 입력 값에 따라 plus 버튼 enable/disable

--- app/src/main/java/com/example/naengteol_recipe/MainActivity.kt
+++ app/src/main/java/com/example/naengteol_recipe/MainActivity.kt
@@ -9,7 +9,6 @@ class MainActivity : AppCompatActivity() {
     private val binding get() = mBinding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
 
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
--- app/src/main/java/com/example/naengteol_recipe/RefrigeratorFrag.kt
+++ app/src/main/java/com/example/naengteol_recipe/RefrigeratorFrag.kt
@@ -1,16 +1,44 @@
 package com.example.naengteol_recipe
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.naengteol_recipe.databinding.FragRefrigeratorBinding
+
 
 class RefrigeratorFrag: Fragment() {
+    private var mBinding : FragRefrigeratorBinding? = null
+    private val binding get() = mBinding!!
+
+    private var foodFlag = false
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        mBinding = FragRefrigeratorBinding.inflate(inflater, container,false)
+        return binding.root
+    }
 
-        val view = inflater.inflate(R.layout.frag_refrigerator, container, false)
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        return view
+        binding.edtFood.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {
+                if(binding.edtFood.text.isNotEmpty()) {
+                    binding.btnCirclePlus.setImageResource(R.drawable.ic_plus_white)
+                    binding.btnCirclePlus.setBackgroundResource(R.drawable.button_linear_enable)
+                    foodFlag = true
+                } else {
+                    binding.btnCirclePlus.setImageResource(R.drawable.ic_plus_gray)
+                    binding.btnCirclePlus.setBackgroundResource(R.drawable.button_linear_disable)
+                    foodFlag = false
+                }
+            }
+        })
     }
 }
